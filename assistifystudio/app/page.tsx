8198f4bc6f7108a9d4d6ce84fb2d4445bcdde5d0
Abhay Pratap Singh <138395064+Abhiolics@users.noreply.github.com>
2024-06-14 18:26:03 +0530

    jj

--- assistifystudio/app/page.tsx
+++ assistifystudio/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero"
 import Animatedb from "@/components/Animatedb"
 import About from "@/components/About"
 import ComingSoon from "@/components/ComingSoon"
+import Testimonials from "@/components/Testimonials"
 import Feature from "@/components/Feature"
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
   <Hero/>
   <About/>
   <Feature/>
+  {/* <Testimonials/> */}
   </main>
   
   </>
